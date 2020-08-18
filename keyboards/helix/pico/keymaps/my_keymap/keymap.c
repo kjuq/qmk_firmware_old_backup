d615a144585c742b655339c78a5558da51d14292
kjuq <44694396+kjuq@users.noreply.github.com>
2020-08-18 22:25:10 +0900

    T cursor movement keymap

--- keyboards/helix/pico/keymaps/my_keymap/keymap.c
+++ keyboards/helix/pico/keymaps/my_keymap/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      KC_TAB,  ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, FNCTN,   KC_RCTL \
+      KC_TAB,  ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  _______, _______, KC_SPC,  RAISE,   KC_RGUI, KC_RALT, FNCTN,   KC_RCTL \
       ),
 
   [_COLEMAK] = LAYOUT( \
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( \
       KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      _______, KC_HOME, _______, _______, KC_END,  KC_DEL,                    KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
-      _______, _______, _______, _______, _______, _______,                   CTRLK,   KC_ENT,  _______, _______, _______, _______, \
+      _______, _______, KC_HOME, KC_END,  _______, KC_DEL,                    KC_BSPC, KC_ENT,  KC_UP,   _______, _______, _______, \
+      _______, _______, _______, _______, _______, _______,                   CTRLK,   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
 
