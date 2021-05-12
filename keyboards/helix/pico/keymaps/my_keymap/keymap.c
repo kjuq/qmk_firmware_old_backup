6bfbd77785fe7020ace7a239d4a5bedec68f9442
kjuq <44694396+kjuq@users.noreply.github.com>
2021-05-13 05:44:14 +0900

    09 on the left

--- keyboards/helix/pico/keymaps/my_keymap/keymap.c
+++ keyboards/helix/pico/keymaps/my_keymap/keymap.c
@@ -56,27 +56,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_TAB,  KC_BSPC, \
       KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    FNCTN, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_K,    KC_M,    _______, _______, SYMBL,   _______, \
-      _______, _______, KC_LALT, CMD_EIS, LOWER,   KC_SPC,  _______, _______, KC_SPC,  KC_RSFT, CMD_KNA, KC_RALT, FNCTN,   _______ \
+      _______, _______, KC_LALT, CMD_EIS, LOWER,   KC_SPC,  _______, _______, KC_SPC,  KC_RSFT, CMD_KNA, KC_RALT, _______, _______ \
       ),
 
   [_LOWER] = LAYOUT( \
       _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,                   KC_BSPC, KC_ENT,  KC_UP,   KC_DEL,  KC_BSLS, _______, \
-      _______, KC_SCLN, KC_SLSH, KC_COMM, KC_DOT,  KC_GRV,                    CTRLK,   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+      _______, KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSLS,                   KC_BSPC, KC_ENT,  KC_QUOT, KC_MINS, KC_EQL,  _______, \
+      _______, KC_SCLN, KC_SLSH, KC_COMM, KC_DOT,  KC_GRV,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
 
   [_SYMBL] = LAYOUT( \
       _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______, \
-      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQUO,                   KC_PIPE, KC_LPRN, KC_RPRN, _______, _______, _______, \
+      _______, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PIPE,                   _______, KC_PLUS, KC_DQUO, KC_UNDS, _______, _______, \
       _______, KC_COLN, KC_QUES, KC_LABK, KC_RABK, KC_TILD,                   _______, _______, _______, _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
 
   [_FN] = LAYOUT( \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                   _______, _______, _______, _______, RESET,   _______, \
-      _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                   _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, _______, \
-      _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                   _______, KC_BRID, KC_BRIU, KC_MPRV, KC_MNXT, _______, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, _______, _______, _______, RESET,   _______, \
+      _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, _______, \
+      _______, KC_F11,  KC_F12,  _______, _______, _______,                   _______, KC_BRID, KC_BRIU, KC_MPRV, KC_MNXT, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
 };
