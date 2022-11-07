36fc95a9f3a722c7967d43dc2d720791d1eef5ee
kjuq <44694396+kjuq@users.noreply.github.com>
2022-11-07 13:02:23 +0900

    added a QWERTY layout and a script for build

--- keyboards/helix/pico/keymaps/my_keymap/keymap.c
+++ keyboards/helix/pico/keymaps/my_keymap/keymap.c
@@ -50,20 +50,29 @@ enum macro_keycodes {
 #define SYMBL2 MO(_SYMBL2)
 #define EMACS  MO(_EMACS)
 #define FNCTN  MO(_FN)
+#define COLEMK DF(_COLEMAK)
+#define QWRTY  DF(_QWERTY)
 
 //Macros
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_COLEMAK] = LAYOUT( \
-		KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_TAB,  KC_BSPC, \
+		KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    _______, KC_BSPC, \
 		KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    FNCTN, \
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_K,    KC_M,    _______, _______, SYMBL,   _______, \
 		KC_LCTL, _______, KC_LALT, CMD_EIS, LOWER,   KC_SPC,  _______, _______, KC_SPC,  KC_RSFT, CMD_KNA, KC_RALT, _______, _______ \
 	),
 
+	[_QWERTY] = LAYOUT( \
+		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    _______, FNCTN, \
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    _______, _______, SYMBL,   _______, \
+		KC_LCTL, _______, KC_LALT, CMD_EIS, LOWER,   KC_SPC,  _______, _______, KC_SPC,  KC_RSFT, CMD_KNA, KC_RALT, _______, _______ \
+	),
+
 	[_LOWER] = LAYOUT( \
-		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TAB, \
 		_______, KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSLS,                   KC_BSPC, KC_ENT,  KC_UP,   KC_MINS, KC_EQL,  _______, \
 		_______, KC_SCLN, KC_SLSH, KC_COMM, KC_DOT,  KC_GRV,                    KC_QUOT, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -77,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 
 	[_FN] = LAYOUT( \
-		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, _______, _______, _______, RESET,   _______, \
+		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, COLEMK,  QWRTY,   _______, RESET,   _______, \
 		_______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, _______, \
 		_______, KC_F11,  KC_F12,  _______, _______, _______,                   _______, KC_BRID, KC_BRIU, KC_MPRV, KC_MNXT, _______, \
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
